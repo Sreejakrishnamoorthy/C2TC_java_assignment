7a8766a82ebf3b881a4f7b51ea1f8ace44be37d8
sreeja <sreejakrishnamoorthy2004@gmail.com>
2025-08-20 21:41:02 +0530

    bankingsystem

---
+++ src/com/sreeja/bankingSystem/Bank.java
@@ -0,0 +1,15 @@
+package com.sreeja.bankingSystem;
+
+public class Bank {
+	private static int totalAccounts=0;
+	
+	
+	public static void incrementAccount() {
+		totalAccounts++;
+	}
+	
+	public static int getTotalAccounts() {
+		return totalAccounts;
+	}
+
+}
